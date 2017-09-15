584f64db9d0e17d305f76f4f54a72b67ee93b847
chinna06 <chinnakp06@gmail.com>
2017-09-15 08:20:21 +0530

    two

--- chinna/src/test/java/chinna_demo/chinna/AppTest.java
+++ chinna/src/test/java/chinna_demo/chinna/AppTest.java
@@ -1,14 +1,32 @@
 package chinna_demo.chinna;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class AppTest{
 	@Test
-	public static void main()
+	public static void main() throws InterruptedException
     {
-        WebDriver n=new FirefoxDriver();
-         n.get("https://www.guru99.com/");
-         System.out.println();    }
+WebDriver driver = new FirefoxDriver();
+		
+		//Launching Sample site
+		driver.get("http://artoftesting.com/sampleSiteForSelenium.html");
+		
+		//Right click in the TextBox
+		Actions action = new Actions(driver);
+		WebElement searchBox = driver.findElement(By.xpath(".//*[@id='commonWebElements']/p[1]/a"));
+		Thread.sleep(3000);
+		action.contextClick(searchBox).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.RETURN).build().perform();
+		
+		//Thread.sleep just for user to notice the event
+		
+		
+		//Closing the driver instance
+		//driver.quit();
+    }
    }

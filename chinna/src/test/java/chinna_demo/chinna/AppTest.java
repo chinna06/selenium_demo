f3468639ce7c6feac7b0483819d0983729fedf6d
chinna06 <chinnakp06@gmail.com>
2017-09-28 07:13:19 +0530

    chin_new

--- chinna/src/test/java/chinna_demo/chinna/AppTest.java
+++ chinna/src/test/java/chinna_demo/chinna/AppTest.java
@@ -2,10 +2,13 @@ package chinna_demo.chinna;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.NoSuchWindowException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class AppTest{
@@ -13,16 +16,21 @@ public class AppTest{
 	public static void main() throws InterruptedException
     {
 WebDriver driver = new FirefoxDriver();
-		
 		//Launching Sample site
 		driver.get("http://artoftesting.com/sampleSiteForSelenium.html");
-		
+		try{
 		//Right click in the TextBox
 		Actions action = new Actions(driver);
-		WebElement searchBox = driver.findElement(By.xpath(".//*[@id='commonWebElements']/p[1]/a"));
+		WebElement searchBox = driver.findElement(By.xpath(".//*[@id='commonWebElemen']/p[1]/a"));
+		
+		
+
 		Thread.sleep(3000);
 		action.contextClick(searchBox).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.RETURN).build().perform();
-		
+		}
+		catch(NoSuchWindowException e){
+			
+		}
 		//Thread.sleep just for user to notice the event
 		
 		
